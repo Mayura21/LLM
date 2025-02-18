102cb85d0247d2d9ef3faa5f8257e43670178f05
Mayura21 <mayurasm2000@gmail.com>
2025-02-18 21:40:09 +0530

    Regression model updated

--- 13_ANN/streamlit_regression.py
+++ 13_ANN/streamlit_regression.py
@@ -27,7 +27,7 @@ gender =st.selectbox("Gender", label_encoder_gender.classes_)
 age = st.slider('Age', 18, 92)
 balance = st.number_input('Balance')
 credit_score = st.number_input("Credit score")
-estimated_salary = st.number_input("Estimated salary")
+exited = st.number_input("Exited", [0, 1])
 tenure = st.slider('Tenure', 0, 10)
 num_of_products = st.slider("Number of Products", 1, 4)
 has_cr_card = st.selectbox("Has Credit Card", [0, 1])
@@ -44,7 +44,7 @@ input_data = pd.DataFrame({
     'NumOfProducts': [num_of_products],
     'HasCrCard': [has_cr_card],
     'IsActiveMember': [is_active_member],
-    'EstimatedSalary': [estimated_salary]
+    'Exited': [exited]
 })
 
 # One hot encode 'Geography'
@@ -61,9 +61,9 @@ input_data_scaled = scaler.transform(input_data)
 prediction = model.predict(input_data_scaled)
 prediction_proba = prediction[0][0]
 
-st.write(f"Churn Probability: {prediction_proba}")
+st.write(f"Salary: {prediction_proba}")
 
-if prediction_proba > 0.5:
-    st.write('The customer is likely to churn.')
-else:
-    st.write('The customer is not likely to churn.')
+# if prediction_proba > 0.5:
+#     st.write('The customer is likely to churn.')
+# else:
+#     st.write('The customer is not likely to churn.')
