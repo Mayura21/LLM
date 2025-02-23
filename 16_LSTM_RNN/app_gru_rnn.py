7383e33e4866d061d7a094aedf9bf17f7eb532da
Mayura21 <mayurasm2000@gmail.com>
2025-02-23 17:04:00 +0530

    LSTM rnn implementaion

---
+++ 16_LSTM_RNN/app_gru_rnn.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import numpy as np
+import pickle
+from tensorflow.keras.models import load_model # type: ignore
+from tensorflow.keras.preprocessing.sequence import pad_sequences # type: ignore
+
+# Load LSTM model
+model = load_model('16_LSTM_RNN/next_word_gru_rnn.h5')
+
+# Load tokenizer
+with open('16_LSTM_RNN/tokenizer_gru_rnn.pickle', 'rb') as file:
+    tokenizer = pickle.load(file)
+
+
+# Function to predict next word
+def predict_next_word(model, tokenizer, text, max_sequence_len):
+    token_list = tokenizer.texts_to_sequences([text])[0]
+
+    if len(token_list) >= max_sequence_len:
+        token_list = token_list[-(max_sequence_len-1): ]        # Ensure sequence length matches max_sequence_len
+    
+    token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+    predicted = model.predict(token_list, verbose=0)
+    predicted_word_index = np.argmax(predicted, axis=1)
+    for word, index in tokenizer.word_index.items():
+        if index == predicted_word_index:
+            return word
+        
+    return None
+
+# Streamlit app
+st.title("Next word prediction with LSTM and early stopping")
+input_text = st.text_input("Enter the sequence of words", "To be or not to")
+if st.button("Predict Next Word"):
+    max_sequence_len = model.input_shape[1] + 1
+    next_word = predict_next_word(model, tokenizer, input_text, max_sequence_len)
+    st.write(f'Next word: {next_word}')
